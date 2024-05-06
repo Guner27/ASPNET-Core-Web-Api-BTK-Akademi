3a1dc9d09550574367aa03dfaab0be7fbc3af797
guner <guner@MSI>
2024-05-06 23:39:51 +0300

    Add project files.

---
+++ basics/HelloWebApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HelloWebApi.Models;
+
+namespace HelloWebApi.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            var result = new ResponseModel()
+            {
+                HttpStatus=200,
+                Message ="Hello ASP.NET success"
+            };
+            return Ok(result);
+        }
+    }
+}
