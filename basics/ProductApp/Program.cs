524684642ccf5a45ba075e860c3f352d62119df7
Muhammed GÜNER <guner.muhammed@outlook.com>
2024-05-07 17:12:31 +0300

    Logging

---
+++ basics/ProductApp/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();//Consola Log düþme
+builder.Logging.AddDebug();//Debuga Log düþme
+
+var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
