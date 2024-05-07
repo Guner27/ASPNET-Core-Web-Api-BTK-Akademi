ab32504d01153008e42ca66b0621cf59db9ebf81
Muhammed GÜNER <guner.muhammed@outlook.com>
2024-05-07 20:04:01 +0300

    Working with Models and HTTP methods

---
+++ bookDemo/Data/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using bookDemo.Models;
+
+namespace bookDemo.Data
+{
+    public static class ApplicationContext
+    {
+        public static List<Book> Books { get; set; }
+        static ApplicationContext()
+        {
+            Books = new List<Book>() {
+             new Book(){Id =1,Title="Karagöz ve Hacivat", Price=78 },
+            new Book(){Id =2,Title="Mesnevi", Price=100 },
+            new Book(){Id =3,Title="Prince Of Persia", Price=52 }};
+        }
+    }
+}
