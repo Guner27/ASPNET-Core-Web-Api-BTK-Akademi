45b57dce4152cdab4e2ed451903f2e0416dabe9c
Muhammed GÜNER <guner.muhammed@outlook.com>
2024-05-13 13:21:59 +0300

    Content Negotiation (İçerik Pazarlığı)

---
+++ bsStoreApp/Entities/DataTransferObjects/BookDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.DataTransferObjects
+{
+    public record BookDto
+    {
+        public int Id { get; init; } //init anahtar sözcüğü Readonly  özellik kazandırır.
+        public String Title { get; init; }
+        public decimal Price { get; init; }
+    }
+}
--- bsStoreApp/WebApi/Program.cs
+++ bsStoreApp/WebApi/Program.cs
@@ -3,48 +3,60 @@ using Services.Contracts;
 using WebApi.Extensions;
 
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddControllers()
-    .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
-    .AddNewtonsoftJson();
-
-LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
-
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.ConfigureSqlContext(builder.Configuration);
-builder.Services.ConfigureRepositoryManager();
-builder.Services.ConfigureServiceManager();
-builder.Services.ConfigureLoggerService();
-builder.Services.AddAutoMapper(typeof(Program));
-
-
-var app = builder.Build();
-
-var logger = app.Services.GetRequiredService<ILoggerService>();
-app.ConfigExceptionHandler(logger);
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddControllers(config =>
+        {
+            config.RespectBrowserAcceptHeader = true; //Ýçerik pazarlýðýna uygulama açýldý.
+            config.ReturnHttpNotAcceptable = true;
+        })
+            .AddCustomCsvFormatter()
+            .AddXmlDataContractSerializerFormatters()
+            .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
+            .AddNewtonsoftJson();
+
+        LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.ConfigureSqlContext(builder.Configuration);
+        builder.Services.ConfigureRepositoryManager();
+        builder.Services.ConfigureServiceManager();
+        builder.Services.ConfigureLoggerService();
+        builder.Services.AddAutoMapper(typeof(Program));
+
+
+        var app = builder.Build();
+
+        var logger = app.Services.GetRequiredService<ILoggerService>();
+        app.ConfigExceptionHandler(logger);
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        if (app.Environment.IsProduction())
+        {
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-if (app.Environment.IsProduction())
-{
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
