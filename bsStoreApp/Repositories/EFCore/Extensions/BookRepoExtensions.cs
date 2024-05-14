b6f3f1bf1a4ebe5ce893a442ad2e57a3ee68e8fd
Muhammed GÜNER <guner.muhammed@outlook.com>
2024-05-14 22:49:49 +0300

    Search (Arama)

---
+++ bsStoreApp/Repositories/EFCore/Extensions/BookRepoExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore.Extensions
+{
+    public static class BookRepoExtensions
+    {
+        public static IQueryable<Book> FilterBooks(this IQueryable<Book> books,
+            uint minPrice, uint maxPrice) =>
+            books.Where(book =>
+            book.Price >= minPrice &&
+            book.Price <= maxPrice);
+
+        public static IQueryable<Book> Search(this IQueryable<Book> books, string searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+                return books;
+
+            //Trim : Başında sonunda boşluklar varsa onları at.
+            //ToLower : Tüm harfleri küçükharf yap.
+            var lowerCaseTerm = searchTerm.Trim().ToLower();
+            return books.Where(book => book.Title
+                .ToLower()
+                .Contains(searchTerm));
+        }
+    }
+}
