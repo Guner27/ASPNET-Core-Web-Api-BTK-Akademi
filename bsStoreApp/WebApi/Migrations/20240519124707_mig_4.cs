256355843bc6ee68df73cb08cc7c94c51f79ac7a
Muhammed GÜNER <guner.muhammed@outlook.com>
2024-05-19 15:48:55 +0300

    RefreshToken Fields

---
+++ bsStoreApp/WebApi/Migrations/20240519124707_mig_4.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1e7fa948-a324-48cf-bc0f-d24de73a36dd");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c75688cd-d01b-46d1-80fa-19d3e0bce9b7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e6fe486b-7d13-4207-950a-031760e4e5b0");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "b269d45b-13e7-4ca9-abdb-661c0126b2b7", null, "Admin", "ADMIN" },
+                    { "cf188404-129e-4aec-88f9-d9998ae804e7", null, "Editor", "EDITOR" },
+                    { "d4cc4ce8-c8b6-4457-b153-8034bf17b258", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b269d45b-13e7-4ca9-abdb-661c0126b2b7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cf188404-129e-4aec-88f9-d9998ae804e7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d4cc4ce8-c8b6-4457-b153-8034bf17b258");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1e7fa948-a324-48cf-bc0f-d24de73a36dd", null, "Admin", "ADMIN" },
+                    { "c75688cd-d01b-46d1-80fa-19d3e0bce9b7", null, "User", "USER" },
+                    { "e6fe486b-7d13-4207-950a-031760e4e5b0", null, "Editor", "EDITOR" }
+                });
+        }
+    }
+}
