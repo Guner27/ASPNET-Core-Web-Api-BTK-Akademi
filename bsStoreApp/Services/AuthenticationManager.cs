9ba1ef8b8d05507fa2543c9d022e4e16863cd5c6
Muhammed GÜNER <guner.muhammed@outlook.com>
2024-05-19 12:06:04 +0300

    IAuthenticationService

---
+++ bsStoreApp/Services/AuthenticationManager.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Services.Contracts;
+
+namespace Services
+{
+    public class AuthenticationManager : IAuthenticationService
+    {
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _config;
+
+        public AuthenticationManager(
+            ILoggerService logger,
+            IMapper mapper,
+            UserManager<User> userManager,
+            IConfiguration config)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+            _config = config;
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
