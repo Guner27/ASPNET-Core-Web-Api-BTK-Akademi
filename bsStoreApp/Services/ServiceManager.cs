734d334556955a1d981411a12f44ae768d2d004c
Muhammed GÜNER <guner.muhammed@outlook.com>
2024-05-20 20:02:01 +0300

    Service Manager

--- bsStoreApp/Services/ServiceManager.cs
+++ bsStoreApp/Services/ServiceManager.cs
@@ -16,7 +16,9 @@ namespace Services
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IBookService> _bookService;
+        private readonly Lazy<ICategoryService> _categoryService;
         private readonly Lazy<IAuthenticationService> _authenticationService;
+        
 
         public ServiceManager(
             IRepositoryManager repositoryManager,
@@ -26,12 +28,19 @@ namespace Services
             UserManager<User> userManager,
             IBookLinks bookLinks)
         {
-            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, logger, mapper, bookLinks));
+            _bookService = new Lazy<IBookService>(() => 
+            new BookManager(repositoryManager, logger, mapper, bookLinks));
+
+            _categoryService = new Lazy<ICategoryService> (() =>
+            new CategoryManager(repositoryManager));
+
             _authenticationService = new Lazy<IAuthenticationService>(() =>
             new AuthenticationManager(logger, mapper, userManager, configuration));
         }
         public IBookService BookService => _bookService.Value;
 
         public IAuthenticationService AuthenticationService => _authenticationService.Value;
+
+        public ICategoryService CategoryService => _categoryService.Value;
     }
 }
